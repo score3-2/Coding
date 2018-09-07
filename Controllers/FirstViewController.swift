1586536bf2011b4e37054f0be2be23c137b4c3c3
Abel P <abelomar@icloud.com>
2018-09-07 13:46:29 -0700

    Slider
    Slider + Label

--- Controllers/FirstViewController.swift
+++ Controllers/FirstViewController.swift
@@ -9,26 +9,46 @@
 import UIKit
 
 class FirstViewController: UIViewController, UITextFieldDelegate {
+    
+    // Properties
+    var userAge = -1
+    
+        // Conections
+    @IBOutlet weak var labelAge: UILabel!
+    @IBOutlet weak var sliderAge: UISlider!
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+      
+        self.updateLabel()
+        
     }
     
-    // Methods
+    //MARK:- Methods
     
+    // Keyboard Dismissal
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         
         textField.resignFirstResponder()
         print("You Pressed Done")
-        
         if let textImput = textField.text {
             print(textImput)
         }
         
         return true
     }
-
+    
+    // Slider Method
+    @IBAction func sliderAgeMoved(_ sender: UISlider) {
+        self.updateLabel()
+        }
+    
+    // Update Label
+    func updateLabel() {
+        self.userAge = Int(self.sliderAge.value)
+        self.labelAge.text = String(self.userAge)
+    }
 
 }
 
